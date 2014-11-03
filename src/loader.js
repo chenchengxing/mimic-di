237de14328c3e57667c9f6ba870ccb66f9be7af7
chenchengxing <ccxsungodzzx@gmail.com>
2014-11-03 17:20:45 +0800

    completed

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,13 @@
-function hello() {
-  return 'hello';
+function setupModuleLoader (window) {
+  function ensure (obj, name, factory) {
+    return obj[name] = obj[name] || factory();
+  }
+
+  var angular = ensure(window, 'angular', Object);
+
+  ensure(angular, 'module', function () {
+    return function () {
+      
+    };
+  });
 }
--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,7 +1,33 @@
 var expect = chai.expect;
 
-describe('hello, test', function () {
-  it('should return hello', function () {
-    expect(hello()).to.equal('hello');
+describe('setupModuleLoader', function() {
+
+  beforeEach(function () {
+    delete window.angular;
+  })
+
+  it('exposes angular on the window', function() {
+    setupModuleLoader(window);
+    expect(window.angular).to.exist;
+  });
+
+  it('create angular just once', function () {
+    setupModuleLoader(window);
+    var ng = window.angular;
+    setupModuleLoader(window);
+    expect(window.angular).to.equal(ng);
+  });
+
+  it('defines angular.module', function() {
+    setupModuleLoader(window);
+    expect(window.angular.module).to.exist;
+  });
+
+  it('should create angular.module just once', function() {
+    setupModuleLoader(window);
+    var module = angular.module;
+    setupModuleLoader(window);
+    expect(angular.module).to.equal(module);
   });
 });
+
